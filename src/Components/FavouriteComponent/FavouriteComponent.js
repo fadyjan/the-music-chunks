a1001a45a687138e4d49325e33e29cf79063b536
fadyjan <fady.jan95@gmail.com>
2021-09-18 18:52:10 +0200

    Create #3 Html Architecture of Favourite Tracks Component in the middle of the screen

--- src/Components/FavouriteComponent/FavouriteComponent.js
+++ src/Components/FavouriteComponent/FavouriteComponent.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import AdeleCover from '../../Assets/Adele.jpg';
+import be from '../../Assets/believer.jpg';
+import DrakeImage from "../../Assets/drake.jpg";
+import  "./FavouriteComponent.css" ;
+
 
 function FavouriteComponent() {
-    return (
-        <div>
-            FavouriteComponent
-        </div>
-    )
+    
+  return (
+
+        <img
+          className="CoverImagess"
+          src={AdeleCover}
+
+        />
+
+      
+  );
 }
 
-export default FavouriteComponent
+export default FavouriteComponent;
